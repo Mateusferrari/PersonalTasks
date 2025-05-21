7c8165ee5b47501d5a82c2088deb189c7a57c17a
Mateus Ferrari <mateusfaa@hotmail.com>
2025-05-21 17:51:34 -0300

    feat: entidade Task com atributos e anotações

---
+++ app/src/main/java/com/mateus/personaltasks/model/Task.kt
@@ -0,0 +1,12 @@
+package com.seunome.personaltasks.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    var title: String,
+    var description: String,
+    var deadline: String
+)
