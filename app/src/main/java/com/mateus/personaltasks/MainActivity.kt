68989de860cf67098048b070666842162b98df61
Mateus Ferrari <mateusfaa@hotmail.com>
2025-05-22 16:38:17 -0300

    feat: menu de opções com item Nova Tarefa

--- app/src/main/java/com/mateus/personaltasks/MainActivity.kt
+++ app/src/main/java/com/mateus/personaltasks/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.mateus.personaltasks.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mateus.personaltasks.R
@@ -28,4 +31,20 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerViewTasks.layoutManager = LinearLayoutManager(this)
         binding.recyclerViewTasks.adapter = adapter
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_add -> {
+                Toast.makeText(this, "Nova Tarefa clicada", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
